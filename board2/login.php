4eece1c36a04a84598c93304e1ca4efd833afa7f
unknown <juice.5959.atsu@gmail.com>
2018-01-18 18:58:49 +0900

    0118

--- board2/login.php
+++ board2/login.php
@@ -6,19 +6,19 @@ session_start();
 $_SESSION["name_login"] = $_POST["name_login"];
 $_SESSION["password_login"] = $_POST["password_login"];
 
+$name=$_POST['name_login'];
+$password=$_POST['password_login'];
+
 try{
     $db=getDb();
-    $a = $db->prepare('SELECT * FROM member_table WHERE name = ? AND password = ?');
-    $c = $db->prepare('SELECT id FROM member_table WHERE name = ?');
-    $name=$_POST['name_login'];
-    $password=$_POST['password_login'];
-    $a->bindValue(1, $name, PDO::PARAM_STR);
-    $a->bindValue(2, $password, PDO::PARAM_STR);
-    $c->bindValue(1, $_SESSION["name_login"], PDO::PARAM_STR);
-    $b= $a->execute();
-    $_SESSION["user_id"] = $c->execute();
+    $stmt = $db->prepare("SELECT id FROM member_table WHERE name = '$name' AND password = '$password'");
+    $user_id = $stmt->execute();
+    $_SESSION["user_id"] = $user_id;
 
-    if($b){
+    //var_dump($b);
+    //exit;
+
+    if(isset($user_id)){
         header( "location: afterLogin.php" );
     }
 }catch(PDOException $e){
